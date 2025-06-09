0c5997b602dfd289497837f80951ede55c288fd4
iamDeenko <denis.mahmutovic@stu.ibu.edu.ba>
2025-06-09 18:23:16 +0200

    boze sacuvaj koliko je ovo case sensitive

--- Frontend/assets/utils/Constants.js
+++ Frontend/assets/utils/Constants.js
@@ -2,5 +2,5 @@ let Constants = {
   PROJECT_BASE_URL:
     location.hostname == "petsociety.local"
       ? "http://petsociety.local/api"
-      : "https://lionfish-app-s3jmy.ondigitalocean.app/backend",
+      : "https://petsociety-nwfc4.ondigitalocean.app/petsociety-backend",
 };
